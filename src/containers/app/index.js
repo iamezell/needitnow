449813d04e27dba6d37ce97e74772a67202a8bb7
Ezell Burke <Ezell.Burke@bcbsfl.com>
2017-07-19 06:24:52 -0400

    updating

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
-const App = () => (
-    <div>
-          <header>
-                  <Link to="/">Home</Link>
-        <Link to="/about-us">About</Link>
-      </header>
 
-      <main>
-              <Route exact path="/" component={Home} />
+function handleTouchTap() {
+    alert('onTouchTap triggered on the title component');
+}
+
+
+const App = () => {
+    return (
+        <MuiThemeProvider>
+        <div>
+        <header>
+        <AppBar
+    title='Black Power Games'
+    titleStyle={{textAlign: 'center', font:'Roboto'}}
+    onTitleTouchTap={handleTouchTap}
+    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+    style={{backgroundColor: 'black'}}
+/>
+</header>
+
+    <main>
+    <Route exact path="/" component={Home} />
         <Route exact path="/about-us" component={About} />
-      </main>
-    </div>
+        </main>
+        </div>
+        </MuiThemeProvider>
 )
+}
 export default App
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -2,44 +2,68 @@ import React from 'react'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import {
-    increment,
-    incrementAsync,
-    decrement,
-    decrementAsync
-} from '../../modules/counter'
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import {Grid} from 'react-flexbox-grid/lib';
+
+
+
+const styles = {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+    gridList: {
+        overflowY: 'auto',
+    },
+};
+
+const tilesData = [
+    {
+        img: 'images/grid-list/00-52-29-429_6401.jpg',
+        title: 'You Don\'t know Black',
+        author: 'Ezell Burke',
+        alt: ''
+    },
+    {
+        img: 'images/grid-list/00-52-29-429_6402.jpg',
+        title: 'You Don\'t know Black',
+        author: 'Ezell Burke',
+        alt: ''
+    }
+]
 
 const Home = props => (
-    <div>
-          <h1>The Big Need</h1>
-      <p>Count: {props.count}</p>
-
-      <p>
-              <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
-        <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
-      </p>
-
-      <p>
-              <button onClick={props.decrement} disabled={props.isDecrementing}>Decrementing</button>
-        <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
-      </p>
-
-      <p><button onClick={() => props.changePage()}>Go to about page via redux</button></p>
-    </div>
+<Grid fluid>
+<div>
+<h2 style={{color:'white', textAlign: 'center'}}>Featured Demo</h2>
+<GridList
+cellHeight={180}
+style={styles.gridList}
+>
+{tilesData.map((tile) => (
+<GridTile
+    key={tile.img}
+    title={tile.title}
+    subtitle={<span>by <b>{tile.author}</b></span>}
+    actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+>
+<img alt='' src={tile.img} />
+</GridTile>
+))}
+</GridList>
+</div>
+</Grid>
 )
 
 const mapStateToProps = state => ({
-    count: state.counter.count,
-    isIncrementing: state.counter.isIncrementing,
-    isDecrementing: state.counter.isDecrementing
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-    increment,
-    incrementAsync,
-    decrement,
-    decrementAsync,
-    changePage: () => push('/about-us')
+        changePage: () => push('/about-us')
 }, dispatch)
 
 export default connect(
