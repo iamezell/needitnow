e19b4ad2a4528775d9218ce6920610ff34f98d76
Ezell Burke <Ezell.Burke@bcbsfl.com>
2017-07-24 18:10:00 -0400

    commit

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,36 +2,45 @@ import React from 'react';
 import { Route } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import { Button, Jumbotron, Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+const jumbotronInstance = (
+    <Jumbotron>
+        <h1>Become an owner!</h1>
+        <p>We use the internet and it's technologies to bring wealth to everyone starting from this very site.</p>
+        <p><Button bsStyle="primary">Learn more</Button></p>
+    </Jumbotron>
+);
 
-
-function handleTouchTap() {
-    alert('onTouchTap triggered on the title component');
-}
-
+const navbarInstance = (
+    <Navbar>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <a href="#">thebigNEED</a>
+            </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+            <NavItem eventKey={1} href="#">About</NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.4}>Separated link</MenuItem>
+            </NavDropdown>
+        </Nav>
+    </Navbar>
+);
 
 const App = () => {
     return (
-        <MuiThemeProvider>
         <div>
-        <header>
-        <AppBar
-    title='thebigNEED'
-    titleStyle={{textAlign: 'center', font:'Roboto'}}
-    onTitleTouchTap={handleTouchTap}
-    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-/>
-</header>
-
-    <main>
-    <Route exact path="/" component={Home} />
-        <Route exact path="/about-us" component={About} />
-        </main>
+            {navbarInstance}
+            {jumbotronInstance}
+            <main>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+            </main>
         </div>
-        </MuiThemeProvider>
 )
 }
 export default App
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -2,10 +2,45 @@ import React, { Component } from 'react'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import {Grid} from 'react-flexbox-grid/lib';
-import FlatButton from 'material-ui/FlatButton';
+import { Button, Thumbnail, Col, Row, Grid} from 'react-bootstrap';
 
+
+const thumbnailInstance = (
+    <Grid>
+        <Row>
+            <Col xs={6} md={4}>
+                <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
+                    <h3>Thumbnail label</h3>
+                    <p>Description</p>
+                    <p>
+                        <Button bsStyle="primary">Button</Button>&nbsp;
+                        <Button bsStyle="default">Button</Button>
+                    </p>
+                </Thumbnail>
+            </Col>
+            <Col xs={6} md={4}>
+                <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
+                    <h3>Thumbnail label</h3>
+                    <p>Description</p>
+                    <p>
+                        <Button bsStyle="primary">Button</Button>&nbsp;
+                        <Button bsStyle="default">Button</Button>
+                    </p>
+                </Thumbnail>
+            </Col>
+            <Col xs={6} md={4}>
+                <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
+                    <h3>Thumbnail label</h3>
+                    <p>Description</p>
+                    <p>
+                        <Button bsStyle="primary">Button</Button>&nbsp;
+                        <Button bsStyle="default">Button</Button>
+                    </p>
+                </Thumbnail>
+            </Col>
+        </Row>
+    </Grid>
+);
 
 export class Home extends Component {
     constructor() {
@@ -16,47 +51,10 @@ export class Home extends Component {
     }
 
 
-handleToggle = (event, toggle) => {
-    this.setState({expanded: toggle});
-};
 
-handleExpand = () => {
-    this.setState({expanded: true});
-};
-
-handleReduce = () => {
-    this.setState({expanded: false});
-};
 
 render() {
-    return (
-            <Grid>
-        <Card>
-        <CardHeader
-    title="Ezell Burke"
-    subtitle="CEO"
-    avatar="images/ezellAvatar.jpg"
-        />
-        <CardMedia
-    overlay={<CardTitle
-            title="thebigNEED"
-            subtitle='We know that the internet is the most powerful tool open to everyone today.We must youthe internet and its technologies to bring wealth to everyone, and empower them to kepp and grow it' />}
->
-<img src="images/finIndpic.jpg" alt="" />
-        </CardMedia>
-        <CardTitle title="Join The Platform" subtitle="Financial Freedom the first basic human right" />
-        <CardText>
-       Straight up and down. The first 1000 members will become owners of this website. We will partner with me to make this site
-    </CardText>
-    <CardActions>
-    <FlatButton label="Action1" />
-        <FlatButton label="Action2" />
-        </CardActions>
-        </Card>
-);
-
-</Grid>
-);
+    return (<div>{thumbnailInstance}</div>);
 }
 }
 
