c8de1afca952f60b4ff29ebc2bb89806eeed06fa
Ezell <iamezell@gmail.com>
2017-08-08 15:36:38 -0400

    adding in meta tags for facebook

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -67,9 +67,7 @@ const uiConfig = {
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
           firebase.auth.FacebookAuthProvider.PROVIDER_ID,
           firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.PhoneAuthProvider.PROVIDER_ID
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
         ],
         // Terms of service url.
         tosUrl: 'platform'
