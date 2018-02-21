575d22d0498bfba7603244e7edf5086914f4503a
Ezell <iamezell@gmail.com>
2018-02-21 13:04:01 -0500

    killing twitter sign up

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -66,7 +66,6 @@ const uiConfig = {
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
           firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
           firebase.auth.EmailAuthProvider.PROVIDER_ID
         ],
         // Terms of service url.
@@ -99,7 +98,7 @@ export class Home extends Component {
 render() {
     return (
         <div>
-         {jumbotronInstance} 
+         {jumbotronInstance}
          <div id="firebaseui-auth-container"></div>
          <div style={{fontSize: '1.5em', fontFamily: 'serif', lineHeight: '1.8em'}}><p>"Lately, I have been thinking about the United Airlines issue with customer service and Uber's scandals involving stealing from Google and sexually harassing women. It reminds me of the saying "You shall know a tree by its fruit" - and their fruit is spread all over the news! All that money and power, it's almost like the more money some companies make the less human their leaders become.</p>
          <br /><p><b style={{fontSize: '2em'}}>Issues like these are a direct consequence of society's  lack of loving and inspirational leaders.</b></p><br /><p>This is why I decided to start the big need, because its gotten so bad that some regular guy like myself must do the impossible and build a company that will show the Facebooks and the Googles of the world how to do it right. Amongst these greedy giants there is just little o'l me and my family. We call ourselves thebigneed. We are stepping onto the scene with a huge ridiculously big and impossible challenge. That is to change the way the economy and society as a whole work. To better it for everyone, from every elderly person to every unborn child.</p> <p>We are the little guys with a mission, a rock and a sling. We are thebigneed. We are Technology leaders who strive to love like King and innovate like Jobs. We who bring social empowerment and justice to the business world, and then execute on it. We seek to inspire and let inspiration be the mechanism of influence not greed. We disrupt conventions like Albert Einstein, We dream of a better world like Marting Luther king jr, We innovate in multiple spectrums like Steve Jobs, We refuse the status quo like Rosa Parks, We guide society to a better place like Harriet Tubman and and invent miracles like Nicola Tesla. We are thebigneed.</p> <p> So even though right now we are a small organization,
